de43b5041b884d52c30ca908568d5c4b06b4c275
AndrewLi <13058100289@163.com>
2019-06-14 01:10:07 +0800

    complete

--- index.js
+++ index.js
@@ -4,13 +4,34 @@
 // 完成下面程序，将`name`变量中的字母全部转为为大写，输出：`'HELLO'`。
 var name = 'hello';
 // TODO write your code here...
+name = name.toUpperCase()
+document.write("name: " + name + "<br/>")
 
 
 // 完成下面程序，将sentence变量中的所有单词首字母变为大写，输出：'Good Afternoon, Mr Mike.'。
 var sentence = 'good afternoon, mr mike.';
-//TODO write your code here...
 
+function up(str) {
+    let arr = new Array()
+    arr = str.split(" ")
+    let newstr = ""
+    newstr = arr.reduce(function (total, cur) {
+        cur = cur.charAt(0).toUpperCase() + cur.substr(1) + " "
+        return total += cur
+    }, "")
+    console.log(newstr)
+    return newstr
+}
+document.write("sentence: " + up(sentence) + "<br/>")
 
 // 完成下面程序，将money变量中的金额提取出来，输出：20。
 var money = '￥20';
 //TODO write your code here...
+function pick(str) {
+    let reg = /\d+/
+    let find = ""
+    find = str.match(reg);
+    return find;
+}
+
+document.write("money num: " + pick(money));
